9b3fa0d1d1da81cabe7b06256f1ad5028bf42a80
Sebastian A Gallardo L <ext_sagallardol@falabella.cl>
2022-01-19 14:59:18 -0300

    clase dos, ejercicios

---
+++ Javascript_curso/Clase_dos/Condicional.js
@@ -0,0 +1,16 @@
+let puntaje_matematica = prompt("Ingresa tu puntaje de la PSU de matemáticas");
+let puntaje_leguaje = prompt("Ingresa tu puntaje de la PSU de lenguaje");
+
+if( puntaje_matematica >= 700 && puntaje_leguaje >= 600){
+    alert('Con tu puntaje lograste entrar a ingeniería civil en informática')
+}
+else if( puntaje_matematica >= 700 && puntaje_leguaje < 600){
+    alert('Tu puntaje de lenguaje es menor al solicitado (600)')
+}
+else if( puntaje_matematica < 700 && puntaje_leguaje > 600){
+    alert('Tu puntaje de matemática es menor al solicitado (700)')
+}
+else{
+    alert('No cumples con los puntajes mínimos')
+}
+
