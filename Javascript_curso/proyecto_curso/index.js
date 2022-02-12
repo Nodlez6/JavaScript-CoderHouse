5dcbef4eb0776aa6702628f8327f77ffcbd2fa9a
nodlez6 <sebastian.gallardo1@mail.udp.cl>
2022-02-12 20:21:52 -0300

    changes project

--- Javascript_curso/proyecto_curso/index.js
+++ Javascript_curso/proyecto_curso/index.js
@@ -1,23 +1,53 @@
-const suma = (a , b) => {
-    return a + b
-}
-const resta = (a , b) => {
-    return a - b
-}
-const division = (a , b) => {
-    return a / b ;
-}
-const multiplicacion = (a , b) => {
-    return a * b;
-}
 
+class Usuario{
+
+    constructor(ID, nombre , email , contrasenia){
+        this._ID = ID;
+        this._nombre = nombre;
+        this._email = email;
+        this._contrasenia = contrasenia;
+    }
+    get email(){
+        return this._email
+    }
+    get nombre(){
+        return this._nombre
+    }
+    get contrasenia(){
+        return this._contrasenia
+    }
+}
 const valorInput = document.getElementById('input-calculadora')
+const historial = document.getElementById('historial__card__contenedor')
+const usuarioReference = document.getElementById('usuario__contenedor')
+
+console.log(historial)
+const usuario = JSON.parse(localStorage.getItem('value'))
+const newUsuario = new Usuario(usuario.id , usuario.nombre, usuario.email , usuario.contrasenia )
+console.log(newUsuario)
+
+const upperCaseFirstLetter = (newUsuario.nombre).charAt(0).toUpperCase() + newUsuario.nombre.slice(1)
+
+const nombre = document.createElement('p')
+nombre.textContent = `Nombre: ${upperCaseFirstLetter}` 
+
+const correo = document.createElement('p')
+correo.textContent = `Correo: ${newUsuario.email}` 
+
+usuarioReference.append(nombre);
+usuarioReference.append(correo);
 
 
+const resuelve = (operacionIngresada) => {
+    operacionIngresada = operacionIngresada.replace('X', '*')
+    return eval(operacionIngresada)
+}
+
+
+
+  
 const numeroCalculadora = (numero) => {
-
     valorInput.value = valorInput.value + numero
-
 
 }
 
@@ -27,8 +57,21 @@ const instruccionCalculadora = (opcion) => {
             valorInput.value = ''
             break;
         case 'Borrar':
+            valorInput.value = valorInput.value.slice(0 , valorInput.value.length - 1)
             break;
         case '=':
+            const temp = valorInput.value
+            valorInput.value = resuelve(valorInput.value)
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('historial__card');
+            const h4Card = document.createElement('h4');
+            h4Card.textContent = temp;
+            const pCard = document.createElement('p')
+            pCard.textContent = valorInput.value
+            
+            historial.append(newDiv)
+            newDiv.append(h4Card)
+            newDiv.append(pCard)
             break;
     }
 
@@ -40,14 +83,14 @@ const operacionCalculadora = (opcion) => {
             valorInput.value = valorInput.value + ' + '
             break;
         case '-':
+            valorInput.value = valorInput.value + ' - '
             break;
         case 'X':
+            valorInput.value = valorInput.value + ' X '
             break;
         case '/':
+            valorInput.value = valorInput.value + ' / '
             break;
     }
     
 }
-
-
-
---
+++ Javascript_curso/proyecto_curso/login.js
@@ -0,0 +1,34 @@
+import data from './data.json' assert {type: "json"}
+
+
+const input_email = document.getElementById("email")
+const input_pass = document.getElementById("password")
+const boton_login = document.getElementById("boton_login")
+
+let dataUsuario;
+
+boton_login.addEventListener("click" , () => {
+  
+    let validUsuario = false
+
+    data.map((usuario) => {
+        if(usuario.email === input_email.value && usuario.contrasenia === input_pass.value){
+            validUsuario = !validUsuario
+            dataUsuario = usuario
+            localStorage.setItem('value' , JSON.stringify(dataUsuario) )
+        }
+    })
+
+    validUsuario ? window.location.pathname = "/coderHouse/Javascript_curso/proyecto_curso/index.html"
+    : console.log("mal ahi")
+
+    input_email.value = ''
+    input_pass.value = ''
+    
+    
+})
+
+export function myDataUsuario(){
+    return dataUsuario;
+}
+
